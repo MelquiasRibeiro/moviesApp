469c87759f776bb1aae9412b7da8a2c6bbd587ff
Melquias Ribeiro <melquias10.mr@gmail.com>
2021-03-07 12:10:23 -0300

    feat: search page

--- src/screens/search/index.js
+++ src/screens/search/index.js
@@ -1,8 +1,60 @@
-import React from 'react';
-import { View } from 'react-native';
+import React,{useState,} from 'react';
+import {Container,Header,SearchInput,Title,Empty,SearchButton } from './styled';
+import {MoviesContainer,Movie,Rate,Poster,Name} from '../home/styles'
+import {Entypo,FontAwesome} from '@expo/vector-icons';
+import search from '../../assets/images/search.png'
+import {api} from '../../services/api'; 
+
+
 
 const Search = () => {
-  return <View />;
+
+  const [movie,setMovie]=useState('');
+  const [results,setResults] = useState([]);
+  
+  const BaseUrlImage='https://image.tmdb.org/t/p/w500';
+
+  function handleGetData() {
+    api.get(`/search/movie?api_key=8d05659f30182cd2011ad0dd54e9f430&language=pt-BR&page=1&include_adult=true&query=${movie}`)
+    .then((respose)=>{
+      setResults(respose.data.results)
+      console.log(respose.data.results)
+
+    }).catch( (err)=>{
+      console.log(err)
+    })
+  }
+
+
+  return (
+  <Container >
+    <Title>Busque por um filme</Title>
+    <Header>
+      <SearchInput onChangeText={
+        (movie) =>setMovie(movie)
+                   }/>
+      <SearchButton onPress={handleGetData}>
+        <FontAwesome name="search" size={16} color="#FFFFFF" />
+      </SearchButton>
+    </Header>
+      <MoviesContainer>
+          { results.length===0?
+            (<Empty source={search}/>) :
+            results.map((film)=>(
+              <Movie key={film.id}>
+                <Poster source={{uri: `${BaseUrlImage}${film.poster_path}`}}/>
+                <Rate>
+                    <Entypo name="star" size={16}  color='#f6a93b'/>
+                    {'  '} 
+                    {`${film.vote_average}/10`}
+                </Rate>
+                <Name>{film.title}</Name>
+              </Movie>
+            ))
+          }
+      </MoviesContainer>    
+  </Container>
+  );
 }
 
 export default Search;
--- src/screens/search/styled.js
+++ src/screens/search/styled.js
@@ -1,5 +1,49 @@
 import styled from 'styled-components/native';
+import { themeColor,secundaryColor,primaryColor,inactiveColor } from '../../styles/colors';
 
-export const Container = styled.View`
-  
+export const Container = styled.SafeAreaView`
+    background-color:${themeColor};
+    flex:1;
+    padding:24px;
+`;
+export const Title =styled.Text`
+    font-size:18px;
+    font-weight:bold;
+    color: ${inactiveColor};
+    margin-top:36px;
+`;
+
+export const Header = styled.View`
+    margin-top:12px;
+    display:flex;
+    flex-direction:row;
+    width:100%;
+`;
+
+export const SearchInput =styled.TextInput`
+    background-color:${secundaryColor};
+    font-size:16px;
+    padding:8px;
+    border-radius:4px;
+    width:85%;
+
+`;
+
+export const Empty = styled.Image`
+    margin-top:150px;
+    height:100px;
+    resize-mode:contain;
+    align-self:center;    
+`;
+
+export const SearchButton = styled.TouchableOpacity`
+    background-color:${primaryColor};
+    font-size:16px;
+    padding:8px 16px;
+    border-radius:4px;
+    margin-left:16px;
+    display:flex;
+    align-items:center;
+    justify-content:center;
+
 `;
